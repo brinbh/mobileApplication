87f8b0cc56d5fe942f65a531a5f9e74026c83c0f
Britt <Britt@Brittany>
2016-10-25 15:18:39 -0600

    Merge origin/master
    Conflicts:
    	murderAtTheMansion/src/byui/cit260/murderAtTheMansion/model/Map.java

--- public_html/JSON Parse Stringify Using XMLHTTP Request.js
+++
@@ -1,7 +0,0 @@
-var students = '{"students" : ['+
-    '{"firstName": "Bailey", "lastName": "Jones", "year": "freshman"},'    
-    '{"firstName": "George", "lastName": "Hall", "year": "sophomore"},'
-    '{"firstName": "Jared", "lastName":"Ford", "year":"freshman"} ]}';
-    
-var obj = JSON.parse(students);
-    
